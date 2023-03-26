5c2b252323f8a2311d4e5190c5c3c76b891defd0
Alexey <aa.reukov@gmail.com>
2023-03-26 19:38:18 +1100

    Init

---
+++ ugc_api/models/models.py
@@ -0,0 +1,31 @@
+import datetime
+import uuid
+
+import orjson
+
+from pydantic import BaseModel
+
+
+def orjson_dumps(val, *, default):
+    return orjson.dumps(val, default=default).decode()
+
+
+class MyBaseModel(BaseModel):
+    class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
+
+ 
+class FilmTimeStamp(MyBaseModel):
+    jwt: str
+    film_id: uuid.UUID
+    film_timestamp: datetime.datetime
+    event_time: datetime.datetime
+
+
+class KafkaFilmTimeStamp(MyBaseModel):
+    user_id: uuid.UUID
+    film_id: uuid.UUID
+    film_timestamp: datetime.datetime
+    event_time: datetime.datetime
+    
